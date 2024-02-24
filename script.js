2045332fbf30a5ef93e41fdfafe0a98db92ac0ad
ridham12 <84904732+ridham12@users.noreply.github.com>
2024-02-24 19:42:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let currentSong = new Audio()
 async function getSongs() {
 
 
-    let a = await fetch("/songs/")
+    let a = await fetch("https://github.com/ridham12/spotify/tree/main/songs")
     let response = await a.text()
     // console.log(response);
     let div = document.createElement("div")
